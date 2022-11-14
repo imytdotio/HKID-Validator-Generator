292817475a5609c4696d6c76098e483388d9559a
imytdotio <imytdotio@gmail.com>
2022-11-15 02:58:16 +0800

    digit check

--- src/Components/Validator.js
+++ src/Components/Validator.js
@@ -16,8 +16,40 @@ export const Validator = (props) => {
   const submitHandler = (e) => {
     e.preventDefault();
     setResult(e.target.value);
-    console.log(char, digit, cDigit);
+    // console.log(char, digit, cDigit);
+    let charSum = 0;
+    // if (char.length == 1) {
+    //   if (char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90) {
+    //     charSum += ((char.charCodeAt(0) - 55) * 8) % 11;
+    //   } else if (char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122) {
+    //     charSum += ((char.charCodeAt(0) - 77) * 8) % 11;
+    //   }
+    // }
+    // // charSum += 3;
+    // console.log(charSum);
+
+    let digitSum = 0;
+    for (let i = 5; i >= 0; i--) {
+      // digitSum += parseInt(digit[7 - i]);
+      if (parseInt(digit[5 - i]) * (i + 2) >= 12) {
+        digitSum += (parseInt(digit[5 - i]) * (i + 2)) % 11;
+      } else {
+        digitSum += parseInt(digit[5 - i]) * (i + 2);
+      }
+    }
+
+    const sum = charSum + digitSum + parseInt(cDigit);
+    if (sum % 11 == 0) {
+      setResult(true);
+    } else {
+      setResult(false);
+    }
+    charSum = char.charCodeAt(0);
+    // console.log(charSum);
+    // console.log(sum);
+    // console.log(result);
   };
+
   useEffect(() => {
     if (char.length >= 1) {
       digitRef.current.focus();
@@ -37,6 +69,7 @@ export const Validator = (props) => {
           onChange={(e) => {
             setChar(e.target.value);
           }}
+          maxLength="2"
         />
         <input
           ref={digitRef}
@@ -45,6 +78,7 @@ export const Validator = (props) => {
           onChange={(e) => {
             setDigit(e.target.value);
           }}
+          maxLength="6"
         />
         <input
           ref={cDigitRef}
@@ -53,6 +87,7 @@ export const Validator = (props) => {
           onChange={(e) => {
             setCDigit(e.target.value);
           }}
+          maxLength="1"
         />
         <button onClick={submitHandler} className="inline-block border">
           Submit
